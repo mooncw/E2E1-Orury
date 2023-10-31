53751d23203337dae058c676e7ff8fb7e2efd6a9
mooncw <dusrktl4@gmail.com>
2023-10-31 23:07:29 +0900

    테스트 코드 구현 연습

--- backend/orury/src/test/java/com/kernel360/orury/OruryApplicationTests.java
+++ backend/orury/src/test/java/com/kernel360/orury/OruryApplicationTests.java
@@ -1,13 +1,91 @@
 package com.kernel360.orury;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kernel360.orury.domain.board.controller.BoardController;
+import com.kernel360.orury.domain.board.db.BoardEntity;
+import com.kernel360.orury.domain.board.db.BoardRepository;
+import com.kernel360.orury.domain.board.model.BoardDto;
+import com.kernel360.orury.domain.board.model.BoardRequest;
+import com.kernel360.orury.domain.board.service.BoardService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class OruryApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	BoardService boardService;
 
+	@Autowired
+	BoardRepository boardRepository;
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper; // JSON 직렬화 및 역직렬화를 위한 ObjectMapper
+
+//	@Test
+//	void createBoardTest() throws Exception {
+//		// 테스트할 데이터를 생성
+//		BoardRequest boardRequest = new BoardRequest();
+//		boardRequest.setBoardTitle("한글 들어감?");
+//
+//		// 컨트롤러 엔드포인트에 POST 요청을 보내고 결과를 검증
+//		MvcResult result = mockMvc.perform(post("/api/board") // 요청 URL을 수정
+//				.contentType(MediaType.APPLICATION_JSON)
+//				.content(objectMapper.writeValueAsString(boardRequest)))
+//			.andExpect(status().isOk())
+//			.andReturn();
+//
+//		// 응답 데이터를 가져와서 검증
+//		String responseContent = result.getResponse().getContentAsString();
+//		System.out.println(responseContent);
+//	}
+
+//	@Test
+//	void createBoardTest() {
+//		BoardRequest boardRequest = new BoardRequest();
+//		boardRequest.setBoardTitle("hard_test2");
+//
+//		BoardDto createBoardDto = boardService.createBoard(boardRequest);
+//
+//		assertEquals(boardRequest.getBoardTitle(), createBoardDto.getBoardTitle(), "불일치");
+//	}
+
+//	@Test
+//	void createBoardTest() {
+//		BoardRequest boardRequest = new BoardRequest();
+//		boardRequest.setBoardTitle("hard_test3");
+//
+//		var entity = BoardEntity.builder()
+//			.boardTitle(boardRequest.getBoardTitle())
+//			.createdBy("admin")  // 임의로 "admin" 넣음
+//			.createdAt(LocalDateTime.now())
+//			.updatedBy("admin") // 임의로 "admin" 넣음
+//			.updatedAt(LocalDateTime.now())
+//			.build();
+//
+//		Long expected = entity.getId();
+//
+//		var saveEntity = boardRepository.save(entity);
+//
+//		Long actual = saveEntity.getId();
+//
+//		assertEquals(expected, actual, "불일치");
+
+//	}
 }
---
+++ backend/orury/src/test/java/com/kernel360/orury/domain/board/BoardTest.java
@@ -0,0 +1,24 @@
+//package com.kernel360.orury.domain.board;
+//
+//
+//import com.kernel360.orury.domain.board.db.BoardEntity;
+//import com.kernel360.orury.domain.board.db.BoardRepository;
+//import com.kernel360.orury.domain.board.service.BoardService;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.test.context.junit.jupiter.SpringExtension;
+//
+//@ExtendWith(SpringExtension.class)
+//public class BoardTest {
+//    @Autowired
+//    private BoardService boardService;
+//    @Autowired
+//    private BoardRepository boardRepository;
+//
+//    @Test
+//    public void createBoard_test() {
+//        boardService.createBoard()
+//    }
+//
+//}
